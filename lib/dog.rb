5819d33d53473f1eda2da4ebc925caa3df36ce5c
Karuna <imkaruna@gmail.com>
2015-11-16 15:24:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def initialize(name, new_breed="Mut")
+    @name = name
+    @breed = new_breed
+  end
+
+  def name=(name)
+    @name=name
+  end
+  def name
+    @name
+  end
+  def breed=(new_breed)
+    @breed=new_breed
+  end
+  def breed
+    @breed
+  end
+end
